1dfc151faa882b74c2ac16af3a46b60cfe2a901c
KartikeyMishra <mkartikey670@gmail.com>
2026-02-01 23:12:53 +0530

    solutions

---
+++ DSA/PracticeQuestions/pc2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> findP(string &s){
+
+}
+int main(){
+    string s;
+    cin >> s;
+    vector<string> ans = findP(s);
+    for(string i: ans){
+        cout << i << " ";
+    }
+    return 0;
+}
